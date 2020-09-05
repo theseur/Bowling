448630919aa33d13dc7bc194ffb98a2358fe5ae8
Tibor <tibor.i.farkas@gmail.com>
2020-09-05 15:37:24 +0200

    Frist BowlingTest

---
+++ src/bowlingtest/Bowling.java
@@ -0,0 +1,10 @@
+package bowlingtest;
+
+import org.junit.Test;
+
+public class Bowling {
+
+
+
+
+}
---
+++ src/bowlingtest/Main.java
@@ -0,0 +1,42 @@
+package bowlingtest;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        BufferedReader sorolvaso = null;
+        ArrayList<Integer> leutottBabuk=  new ArrayList<Integer>();
+        try {
+            sorolvaso = new BufferedReader(new FileReader("Bowling1.txt"));
+            String sor;
+
+
+            while ((sor = sorolvaso.readLine()) != null) {
+                System.out.println(sor.length());
+                int z=sor.length();
+                if(z>1){
+                    for(int i=0;i<z;i++)
+                    leutottBabuk.add(Character.getNumericValue(sor.charAt(i)));
+                }
+            }
+
+
+            sorolvaso.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int sum=0;
+        for(int i=0;i<leutottBabuk.size();i++){
+            System.out.println(leutottBabuk.get(i));
+            sum+=leutottBabuk.get(i);
+            System.out.println("szumma: "+sum);
+        }
+    }
+}
